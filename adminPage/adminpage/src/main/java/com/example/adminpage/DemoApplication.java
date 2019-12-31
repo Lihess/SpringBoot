06dc5cb55f0b468a70749088d7b6b7e982bb1050
Lihess <she6569@gmail.com>
2019-12-31 22:34:54 +0900

    spring boot 설정 완료

---
+++ adminPage/adminpage/src/main/java/com/example/adminpage/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
