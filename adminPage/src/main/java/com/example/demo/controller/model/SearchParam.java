32cdb17300e50a9a7880d24ced9698aac865b189
Lihess <she6569@gmail.com>
2020-01-02 21:01:42 +0900

    lombok : 추가

--- adminPage/src/main/java/com/example/demo/controller/model/SearchParam.java
+++ adminPage/src/main/java/com/example/demo/controller/model/SearchParam.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 @Data
+@AllArgsConstructor
 public class SearchParam{
     private String account;
     private String email;
