2e9d9ff8f2a7fcb533ace4091e9c11b5ab26eea3
Lihess <she6569@gmail.com>
2020-02-03 21:17:51 +0900

    Eatgo : Category 추가2

---
+++ eatGo/eatgo/eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/application/CategoryService.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.eatgo.application;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.fastcampus.eatgo.domain.Category;
+import kr.co.fastcampus.eatgo.domain.CategoryRepository;
+
+@Service
+public class CategoryService{
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+	public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+	public List<Category> getCategories() {
+		List<Category> regions = categoryRepository.findAll();
+        return regions;
+	}
+}
